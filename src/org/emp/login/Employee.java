bf010a340d8dcee2d9abca43a15f4cb825fa9b62
TamilJagan <tamilarasankvl007@gmail.com>
2022-01-27 19:52:26 +0530

    Project structure

---
+++ src/org/emp/login/Employee.java
@@ -0,0 +1,13 @@
+package org.emp.login;
+
+public class Employee {
+	private void empId() {
+		System.out.println("e304343");
+
+	}
+private void empName() {
+	System.out.println("Tamilarasan P.");
+
+}
+
+}
